1056cbb258e605795edfbb3801d0c28a8d035a8f
Sachin Kumar <117358751+sachinkumar118@users.noreply.github.com>
2024-02-20 21:33:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,67 @@
+// Get the number of drum buttons on the page
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+// Loop through each drum button and add a click event listener
 for (var i = 0; i < numberOfDrumButtons; i++) {
-
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
+    // Get the inner HTML (letter) of the clicked drum button
     var buttonInnerHTML = this.innerHTML;
 
+    // Call the makeSound function with the button's inner HTML
     makeSound(buttonInnerHTML);
 
+    // Call the buttonAnimation function with the button's inner HTML
     buttonAnimation(buttonInnerHTML);
-
   });
-
 }
 
+// Add a keypress event listener to the whole document
 document.addEventListener("keypress", function(event) {
-
+  // Call the makeSound function with the pressed key
   makeSound(event.key);
 
+  // Call the buttonAnimation function with the pressed key
   buttonAnimation(event.key);
-
 });
 
-
+// Function to play the corresponding sound based on the key
 function makeSound(key) {
-
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
+      new Audio("sounds/tom-1.mp3").play();
       break;
-
     case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
+      new Audio("sounds/tom-2.mp3").play();
       break;
-
     case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
-      tom3.play();
+      new Audio('sounds/tom-3.mp3').play();
       break;
-
     case "d":
-      var tom4 = new Audio('sounds/tom-4.mp3');
-      tom4.play();
+      new Audio('sounds/tom-4.mp3').play();
       break;
-
     case "j":
-      var snare = new Audio('sounds/snare.mp3');
-      snare.play();
+      new Audio('sounds/snare.mp3').play();
       break;
-
     case "k":
-      var crash = new Audio('sounds/crash.mp3');
-      crash.play();
+      new Audio('sounds/crash.mp3').play();
       break;
-
     case "l":
-      var kick = new Audio('sounds/kick-bass.mp3');
-      kick.play();
+      new Audio('sounds/kick-bass.mp3').play();
       break;
-
-
-    default: console.log(key);
-
+    default:
+      console.log(key);
   }
 }
 
-
+// Function to add and remove the "pressed" class for button animation
 function buttonAnimation(currentKey) {
-
   var activeButton = document.querySelector("." + currentKey);
 
+  // Add the "pressed" class to the active button
   activeButton.classList.add("pressed");
 
+  // Remove the "pressed" class after 100 milliseconds
   setTimeout(function() {
     activeButton.classList.remove("pressed");
   }, 100);
-
 }
